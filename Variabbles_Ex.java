ce1254d3bcd2641cffe0bf69b1de3ae11d819541
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-01-11 14:56:11 +0530

    Instance and static and local ,global

--- Variabbles_Ex.java
+++ Variabbles_Ex.java
@@ -1,6 +1,7 @@
 class Variables_Ex
 {
-	int id=123;//global variable
+	int id=123;//global variable -->instance ,static 4 bytes
+	static int countryCode=91;
 	public static void main(String args[])
 	{
 		byte age=31;//local variable
@@ -9,11 +10,17 @@ class Variables_Ex
 		double result=98.2349236492294;
 		char gender='m';
 		boolean value=true;
-		
-		
+	
 		System.out.println(age+" "+contact+" "+salary+" "+result+" "+gender+" "+value);
-	
-	
+		
+		
+		System.out.println(Variables_Ex.countryCode);
+		Variables_Ex obj=new Variables_Ex();
+		System.out.println(obj.id);
+		
+		
+		
+		
 	}
 
 }

12cc420b450e658091dcaec0f4b596f33c950a7d
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-01-11 10:52:33 +0530

    Day-4

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+class HelloWorld //pascal case 
+{
+	public static void main(String args[])
+	{
+		System.out.println("welcome to java .....");
+	}
+
+}
+
+
+//JDK-->JRE-->JVM--->binary byte code 0's 1's
+
+//translator-->translation
+
+//compiler--->compailation
+
+//source code --->byte code 
+
+//.java--->.class    javac filename.java
+
+//java classname

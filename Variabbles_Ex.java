165bc7df56e6a107c31bf5590336d30553a4636a
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-01-11 12:03:18 +0530

    Day-4

---
+++ Variabbles_Ex.java
@@ -0,0 +1,19 @@
+class Variables_Ex
+{
+	int id=123;//global variable
+	public static void main(String args[])
+	{
+		byte age=31;//local variable
+		long contact=9494348734l;
+		float salary=98.2349236492294f;
+		double result=98.2349236492294;
+		char gender='m';
+		boolean value=true;
+		
+		
+		System.out.println(age+" "+contact+" "+salary+" "+result+" "+gender+" "+value);
+	
+	
+	}
+
+}
